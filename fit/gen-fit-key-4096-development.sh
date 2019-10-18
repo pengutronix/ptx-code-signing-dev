7ec499d8bbaf7f084baa0e711b1d402e24da79aa
Sascha Hauer <s.hauer@pengutronix.de>
2019-10-18 08:39:16 +0200

    Add FIT keys
    The FIT keys are used to sign Kernel FIT images.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

---
+++ fit/gen-fit-key-4096-development.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -e
+
+openssl genrsa -f4 -out fit-4096-development.key 4096
+openssl req -batch -new -days 36500 -x509 -config gen-fit-key-4096-development.conf \
+	-key fit-4096-development.key -out fit-4096-development.crt
+

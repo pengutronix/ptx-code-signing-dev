da1501f158ad196ca0c247441cd8d00f980bf5f1
Jan Luebbe <jlu@pengutronix.de>
2026-04-22 09:28:44 +0200

    fit: add missing command to generate .p12 file
    For consistency, also add this command to the RSA generation script.
    
    Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

--- fit/gen-fit-key-4096-development.sh
+++ fit/gen-fit-key-4096-development.sh
@@ -5,4 +5,6 @@ set -e
 openssl genrsa -f4 -out fit-4096-development.key 4096
 openssl req -batch -new -days 36500 -x509 -config gen-fit-key-4096-development.conf \
 	-key fit-4096-development.key -out fit-4096-development.crt
+openssl pkcs12 -export -inkey fit-4096-development.key -in fit-4096-development.crt \
+	-out fit-4096-development.p12
 
